f12eb786be38a480889c100711e9fd755135adb8
Yifei Sun <ysun@hey.com>
2026-04-06 17:52:09 +0200

    nix: get deps

---
+++ default.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  buildDunePackage,
+  cmdliner,
+  crunch,
+  js_of_ocaml,
+  js_of_ocaml-compiler,
+  js_of_ocaml-ppx,
+  magic-mime,
+}:
+
+buildDunePackage (finalAttrs: {
+  pname = "crouton";
+  version =
+    with lib;
+    pipe ./dune-project [
+      readFile
+      (match ".*\\(version ([^\n]+)\\).*")
+      head
+    ];
+
+  src =
+    with lib.fileset;
+    toSource {
+      root = ./.;
+      fileset = unions [
+        ./bin
+        ./lib
+        ./dune-project
+      ];
+    };
+
+  env.DUNE_CACHE = "disabled";
+
+  propagatedBuildInputs = [
+    cmdliner
+    crunch
+    js_of_ocaml
+    js_of_ocaml-compiler
+    js_of_ocaml-ppx
+    magic-mime
+  ];
+})
